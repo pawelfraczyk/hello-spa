156ee3b08c739de720daf43d1dc8148be16b207f
pawelfraczyk <pawel@fraczyk.eu>
2020-05-13 22:47:28 +0200

    fixing runner

--- Infrastructure/Staging/init_k8smaster.sh
+++ Infrastructure/Staging/init_k8smaster.sh
@@ -28,4 +28,6 @@ rm ./get_helm.sh
 # Install Gitlab runner
 echo "[TASK 6] Install Gitlab runner"
 # Get your URL and Registration Token from GitLab Settings > CI / CD > Runners
+helm repo add gitlab https://charts.gitlab.io/
+helm repo update
 helm install --namespace kube-system gitlab gitlab/gitlab-runner --set gitlabUrl=<gitlabURL>,runnerRegistrationToken=<gitlab-token>,rbac.create=true,rbac.clusterWideAccess=true,runners.namespace=spa
